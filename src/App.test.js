b33d57386a49fd00e0cc3b54db57c93278762ffe
usman23499 <usamn23499@gmail.com>
2021-01-20 02:25:53 +0500

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Check Hello', () => { // set title
+  render(<App />); // kon sa compoent test
+  const linkElement = screen.getByText(/hello/i); // set test data and i mean koe bhi case ho small or large
+  expect(linkElement).toBeInTheDocument(); // hum expect karraha honke ye document pe hai na
 });
+
+
+test("Check World ",()=>{
+  render(<App />);
+  const Check_world= screen.getByText(/world/i);
+  expect(Check_world).toBeInTheDocument();
+})
+
+test('Test placeholder ',()=>{
+  render(<App/>);
+  const place=screen.getByPlaceholderText(/enter name/i);
+  expect(place).toBeInTheDocument();
+})
